55424a54aa665cfac13f8fbe0bf0b987911f08b5
Sheri Zada <sheri.h.zada@gmail.com>
2016-04-04 12:07:56 -0700

    about me transition covers when hidden

--- javascripts/menu.js.jsx
+++ javascripts/menu.js.jsx
@@ -3,8 +3,9 @@ window.Menu = React.createClass({
     return (
       <div className="nav-bar">
         <a href="#" value="ShowAboutMe" onClick={this.props.handleClick}>About Me</a>
-        <a href="#" value="ShowAboutMe" onClick={this.props.handleClick}>Projects</a>
-        <a href="#" value="ShowAboutMe" onClick={this.props.handleClick}>Entente</a>
+        <a href="#" value="ShowProjects" onClick={this.props.handleClick}>Projects</a>
+        <a href="#" value="ShowEntente" onClick={this.props.handleClick}>Entente</a>
+        <a href="#" value="ShowBlog" onClick={this.props.handleClick}>Blog-ish</a>
       </div>
     );
   }
