5df96ad4c313bc12d353b51eba44a91849a02149
Sheri Zada <sheri.h.zada@gmail.com>
2016-04-05 12:30:05 -0700

    fixing them, this time i mean it

--- javascripts/projects.js.jsx
+++ javascripts/projects.js.jsx
@@ -11,13 +11,13 @@ window.Projects = React.createClass({
         <div className="thumbnails">
           <span className="question">A selection of work I have pushed into the Github ether:</span><br />
           <span className="curly-brax">&#123;&#125;</span>Ski Free a vanilla JS game based off of the Microsoft classic.
-          <a href="https://sherizada.com/">[Live]</a><a href="https://github.com/sherzader/ski-free/">[Code]</a><br />
+          <a href="http://sherizada.com/">[Live]</a><a href="https://github.com/sherzader/ski-free/">[Code]</a><br />
           <span className="curly-brax">&#123;&#125;</span>Entente is a web app that allows users to create, edit, and follow groups and search upcoming events using a calendar widget.
-          <a href="https://entente.io/">[Live]</a><a href="https://github.com/sherzader/entente/">[Code]</a><br />
+          <a href="http://entente.io/">[Live]</a><a href="https://github.com/sherzader/entente/">[Code]</a><br />
           <span className="curly-brax">&#123;&#125;</span>AnORM is a variation of Active Record, an object relational mapper.
           <a href="https://github.com/sherzader/AnORM/">[Code]</a><br />
           <span className="curly-brax">&#123;&#125;</span>Instapixies is a web app that uses your hashtag search to fetch photos/videos from Instagram via the Instagram API.
-          <a href="https://instapixies.herokuapp.com/">[Live]</a><a href="https://github.com/sherzader/instapixies/">[Code]</a>
+          <a href="http://instapixies.herokuapp.com/">[Live]</a><a href="https://github.com/sherzader/instapixies/">[Code]</a>
         </div><br />
         <span className="question">What am I currently working on?</span>
           Hello App World!
